b596478ba5d44d2e0b6eb314b9fce249d64c0e81
santiago <santiago@santiago-desktop>
2014-05-13 23:01:45 -0500

    Se documentaron todas las clases y se eliminaron dos metodos que no se estaban usando de la clase contr consultar

--- src/java/Controlador/Contr_Consultar.java
+++ src/java/Controlador/Contr_Consultar.java
@@ -17,6 +17,7 @@ import Modelo.Cls_Satisfaccion;
  */
 public class Contr_Consultar {
 
+    /**Se declaran variables con metodo set y get*/
     String Mensaje;
 
     public String getMensaje() {
@@ -27,107 +28,111 @@ public class Contr_Consultar {
         this.Mensaje = Mensaje;
     }
 
-
-    public String[][] BuscarDatosUsuariosPendientes() {
-        Usuario usu = new Usuario();
-        String[][] Datos = usu.BuscarDatosUsuarioPendientes();
-
-        return Datos;
-    }
-
+    /*Metodo para buscar los datos del usuario*/
     public String[] BuscarDatosUsuario(String Codigo) {
         Usuario usu = new Usuario();
         String[] Datos = usu.getDatosUsuario(Codigo);
-
         return Datos;
     }
 
+    /*Buscar datos de tipo de usuarios*/
+    //pendiente por pasar a ajax
     public String[][] BuscarDatosTipoUsuariosTodos() {
         Tipo_Usuario Tusu = new Tipo_Usuario();
         String[][] Datos = Tusu.BuscarDatosTipoUsuarioTodos();
-
         return Datos;
     }
 
+    /*Buscar los datos de una empresa*/
     public String[][] BuscarDatosEmpresa() {
         Usuario usu = new Usuario();
         String[][] Datos = usu.BuscarDatosEmpresa();
-
         return Datos;
     }
 
+    /*MEtodo apra buscar el blob de la seleccion*/
     public Blob BuscarImagenes(String Codigo) {
         Seleccion sel = new Seleccion();
         Blob Datos = sel.getImagenSeleccion(Codigo);
         return Datos;
     }
 
+    /*MEtodo apra buscar el blob del evento*/
     public Blob BuscarImagenesEvento(String Codigo) {
         Evento eve = new Evento();
         Blob Datos = eve.getImagenEvento(Codigo);
         return Datos;
     }
 
+    /*MEtodo para obtener la cantidad de usuarios empresa pendiente*/
     public String getCantidadPendientes() {
         Usuario usu = new Usuario();
         String Dato = usu.getCantidadPendientes();
         return Dato;
     }
 
+    /*MEtodo para obtener la cantidad de eventos pendientes*/
+    //pendiente por poner en ajax
     public int getCantidadEventosPendientes() {
         Evento eve = new Evento();
         int Dato = eve.CantidadEventoPendiente();
         return Dato;
     }
 
+    /*Metodo para buscar los datos de los proximos eventos*/
+    //pendiente por poner en ajax
     public String[][] getBuscarDatosEventosProximos() {
         Evento eve = new Evento();
         String[][] Datos = eve.BuscarDatosEventosProximos();
         return Datos;
     }
 
+    /*Metodo apra buscar los datos de los eventos destacados*/
+    //pendiente por poner en ajax
     public String[][] getBuscarDatosEventosDestacados() {
         Evento eve = new Evento();
         String[][] Datos = eve.BuscarDatosEventosDestacados();
         return Datos;
     }
 
+    /*MEtodo para buscar los comentarios aleatorios*/
+    //pendiente por poner en ajax
     public String[][] getBuscarComentariosAleatorios() {
         Cls_Satisfaccion sat = new Cls_Satisfaccion();
         String[][] Datos = sat.BuscarComentariosAleatorios();
         return Datos;
     }
 
+    /*Metodo para buscar los datos de los eventos comentados*/
+    //pendiente por poner en ajax
     public String[][] getBuscarDatosEventosComentado() {
         Evento eve = new Evento();
         String[][] Datos = eve.BuscarDatosEventosComentado();
         return Datos;
     }
 
+    /*MEtodo para buscar los datos de un evetno*/
     public String[] getBuscarDatosDetalladosEvento(String CodigoEvento) {
         Evento eve = new Evento();
         String[] Datos = eve.BuscarDatosDetalladosEventos(CodigoEvento);
         return Datos;
     }
     
+    /**Metodo pra buscar los datos de un evento pendiente*/
     public String[] getBuscarDatosDetalladosEventoPendiente(String CodigoEvento) {
         Evento eve = new Evento();
         String[] Datos = eve.BuscarDatosDetalladosEventosPendiente(CodigoEvento);
         return Datos;
     }
 
+    /*Metodo para obtener la calificacion de un evento*/
     public int[] getCalificacionEvento(String CodigoEvento) {
         Evento eve = new Evento();
         int[] Datos = eve.getCalificacionEvento(CodigoEvento);
         return Datos;
     }
 
-    public String[][] getBuscarDatosPrincipalesEventoPendiente() {
-        Evento eve = new Evento();
-        String[][] Datos = eve.BuscarDatosPrincipalesEventosPendientes();
-        return Datos;
-    }
-
+    /**Metodo apra comprobar la calificacion y comentarios de un evetno*/
     public boolean getComprobacionCalificacionYComentario(String CodigoEvento, String CodigoUsuario, String Tipo) {
         boolean b;
         Cls_Satisfaccion sat = new Cls_Satisfaccion();
@@ -135,6 +140,7 @@ public class Contr_Consultar {
         return b;
     }
 
+    /*Metodo para obtener los comentarios o calificacion de un evento*/
     public String getComentarioOCalificacion(String tipo, String CodigoUsuario) {
         Cls_Satisfaccion sat = new Cls_Satisfaccion();
         String dato = sat.getComentarioOCalificacion(tipo, CodigoUsuario);
