9749370191cfcb11bf285df431665330af65b349
santiago <santiago@santiago-desktop>
2014-05-29 11:48:50 -0500

    cambio de lugar para ejecucion remota

--- src/java/Controlador/Contr_Seleccion.java
+++ src/java/Controlador/Contr_Seleccion.java
@@ -50,8 +50,8 @@ public class Contr_Seleccion extends HttpServlet {
             Seleccion sel = new Seleccion();
             String Codigo = "", Mensaje = "", Nombre = "", Tipo = "", Imagen = "", url, Peti;
             String urlsalidaimg;
-            //urlsalidaimg = "/media/santiago/Santiago/IMGTE/";
-            urlsalidaimg = "D:\\IMGTE\\";
+            urlsalidaimg = "/media/santiago/Santiago/IMGTE/";
+            //urlsalidaimg = "D:\\IMGTE\\";
 
             /*FileItemFactory es una interfaz para crear FileItem*/
             FileItemFactory file_factory = new DiskFileItemFactory();
